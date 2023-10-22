bad3b6d73274fb86cec6604ad4bad3d23b483b04
wlamason <lamasonian@gmail.com>
2023-10-22 14:05:54 -0400

    remove temp comment

--- stage_datasets.py
+++ stage_datasets.py
@@ -13,5 +13,4 @@ s3 = boto3.client("s3", endpoint_url="http://localhost:9000", aws_access_key_id=
 for p in tqdm((CWD / "datasets").iterdir()):
     if p.name == ".gitkeep":
         continue
-    relative_path = f"datasets/{p.name}"  # temporary until I migrate my wsl projects to the wsl filesystem instead of mnt
-    s3.upload_file(Filename=relative_path, Bucket="datalake", Key=f"staging/{p.name}")
+    s3.upload_file(Filename=p, Bucket="datalake", Key=f"staging/{p.name}")
